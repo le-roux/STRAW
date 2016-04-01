f3b6a6c336c4fc637e120b22785b56e39b5c4567
Thibault Keriven <Thibault Keriven>
2016-04-01 17:10:31 +0200

    wip

--- STRAW/app/src/main/java/straw/polito/it/straw/CreateMenu.java
+++ STRAW/app/src/main/java/straw/polito/it/straw/CreateMenu.java
@@ -1,13 +1,28 @@
 package straw.polito.it.straw;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class CreateMenu extends AppCompatActivity {
 
+
+
+    private ListView listViewPlate;
+    private Context ctx;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_menu);
+        List<Food> liste_plate= new ArrayList<Food>();
+        liste_plate.add(new Food("Pizza", "4.99", ));
+
+
+        //listViewPlate = ( ListView ) findViewById( R.id._list);
+        listViewPlate.setAdapter(new FoodAdapter(ctx, R.layout.list_design, listViewPlate));
     }
 }
