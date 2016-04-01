3c9c320b80bf8a003e93cb42e1bb19bb479c48cd
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.fr>
2016-04-01 16:52:02 +0200

    Correction of Food class deletion (merge error)

--- STRAW/app/src/main/java/straw/polito/it/straw/food.java
+++ STRAW/app/src/main/java/straw/polito/it/straw/food.java
@@ -1,11 +1,46 @@
 package straw.polito.it.straw;
 
-import android.os.Bundle;
-
 /**
- * Created by tibo on 01/04/2016.
+ * Created by Sylvain on 01/04/2016.
  */
-public class food {
+public class Food {
+    private String name;
+    private float price;
+    private String imageURI;
 
+    public Food(String name, float price, String imageURI) {
+        this.name = name;
+        this.price = price;
+        this.imageURI = imageURI;
+    }
 
+    public Food() {
+        this.name = "Default";
+        this.price = 0f;
+        this.imageURI = null;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public float getPrice() {
+        return this.price;
+    }
+
+    public void setImageURI(String imageURI) {
+        this.imageURI = imageURI;
+    }
+
+    public String getImageURI() {
+        return this.imageURI;
+    }
 }
