f4f3cd96c2693ce66dd1de77da63e71bf0448b09
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.fr>
2016-04-01 15:47:44 +0200

    Creation of the Food class

---
+++ STRAW/app/src/main/java/straw/polito/it/straw/Food.java
@@ -0,0 +1,34 @@
+package straw.polito.it.straw;
+
+/**
+ * Created by Sylvain on 01/04/2016.
+ */
+public class Food {
+    private String name;
+    private float price;
+    private String imageURI;
+
+    public Food(String name, float price, String imageURI) {
+        this.name = name;
+        this.price = price;
+        this.imageURI = imageURI;
+    }
+
+    public Food() {
+        this.name = "Default";
+        this.price = 0f;
+        this.imageURI = null;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public void setImageURI(String imageURI) {
+        this.imageURI = imageURI;
+    }
+}
