4ba639bb69aba34f52960655f9da0d7616c188ac
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.fr>
2016-04-04 15:54:40 +0200

    WIP

--- STRAW/app/src/main/java/straw/polito/it/straw/activities/CreateMenuActivity.java
+++ STRAW/app/src/main/java/straw/polito/it/straw/activities/CreateMenuActivity.java
@@ -1,13 +1,16 @@
 package straw.polito.it.straw.activities;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
-import java.util.List;
 
+import straw.polito.it.straw.data.Drink;
 import straw.polito.it.straw.data.Food;
 import straw.polito.it.straw.adapter.FoodAdapter;
 import straw.polito.it.straw.data.Plate;
@@ -15,21 +18,45 @@ import straw.polito.it.straw.R;
 
 public class CreateMenuActivity extends AppCompatActivity {
 
-
+    private static final int EDIT_FOOD = 1;
+    public static final String ELEMENT = "Element";
 
     private ListView listViewPlate;
-    private Context ctx;
+    private Context context;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_create_menu);
-        ctx=this;
-        List<Food> liste_plate= new ArrayList<Food>();
-        liste_plate.add(new Plate());
+        this.context = this;
+        final ArrayList<Food> list_plate= new ArrayList<Food>();
+        list_plate.add(new Plate());
 
 
 
-        listViewPlate = ( ListView ) findViewById( R.id.Plate_list);
-        listViewPlate.setAdapter(new FoodAdapter(ctx));
+        listViewPlate = (ListView) findViewById( R.id.Plate_list);
+        listViewPlate.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent detail = null;
+                Bundle data = new Bundle();
+                data.putString(ELEMENT, list_plate.get(position).toString());
+                if(list_plate.get(position).getClass() == Drink.class) {
+                    detail = new Intent(getApplicationContext(), Drink.class);
+                } else if (list_plate.get(position).getClass() == Plate.class) {
+                    detail = new Intent(getApplicationContext(), Plate.class);
+                }
+                startActivityForResult(detail, EDIT_FOOD);
+            }
+        });
+        listViewPlate.setAdapter(new FoodAdapter(context, list_plate));
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent result) {
+        if(requestCode == this.EDIT_FOOD) {
+
+        }
     }
 }
