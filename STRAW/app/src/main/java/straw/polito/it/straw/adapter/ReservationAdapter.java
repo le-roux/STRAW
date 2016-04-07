c57fe0b83f711a58fc70478af8993a6c7a9c561c
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.fr>
2016-04-07 18:31:44 +0200

    Filling of the DisplayReservationActivity

---
+++ STRAW/app/src/main/java/straw/polito/it/straw/adapter/ReservationAdapter.java
@@ -0,0 +1,66 @@
+package straw.polito.it.straw.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import straw.polito.it.straw.R;
+import straw.polito.it.straw.data.Reservation;
+
+/**
+ * Created by Sylvain on 07/04/2016.
+ */
+public class ReservationAdapter extends BaseAdapter {
+
+    private ArrayList<Reservation> reservationList;
+    private Context context;
+
+    public ReservationAdapter(Context context) {
+        this.reservationList = new ArrayList<Reservation>();
+        this.context = context;
+    }
+
+    public ReservationAdapter(Context context, ArrayList<Reservation> reservationList) {
+        this.reservationList = reservationList;
+        this.context = context;
+    }
+
+    @Override
+    public int getCount() {
+        return this.reservationList.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return this.reservationList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            LayoutInflater layoutInflater = (LayoutInflater)this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            convertView = layoutInflater.inflate(R.layout.reservation, null);
+        }
+        //Get the views of the item
+        TextView numberPeople = (TextView)convertView.findViewById(R.id.number_people);
+        TextView time = (TextView)convertView.findViewById(R.id.time);
+        TextView plates = (TextView)convertView.findViewById(R.id.plates);
+
+        //Fill them with appropriate values
+        numberPeople.setText(this.reservationList.get(position).getNumberPeople() + context.getString(R.string.Persons));
+        time.setText(this.reservationList.get(position).getTime());
+        plates.setText(this.reservationList.get(position).getPlates());
+
+        return convertView;
+    }
+}
---
+++ STRAW/app/src/main/java/straw/polito/it/straw/data/Reservation.java
@@ -0,0 +1,98 @@
+package straw.polito.it.straw.data;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.sql.Time;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+/**
+ * Created by Sylvain on 07/04/2016.
+ */
+public class Reservation {
+
+    private int numberPeople;
+    private GregorianCalendar time;
+    private String plates;
+
+    public static final String NUMBER_PEOPLE = "NumberPeople";
+    public static final String TIME = "Time";
+    public static final String PLATES = "Plates";
+
+    public Reservation() {
+        this.numberPeople = 0;
+        this.time = new GregorianCalendar();
+        this.plates = "";
+    }
+
+    public Reservation(int numberPeople, String plates) {
+        this.numberPeople = numberPeople;
+        this.plates = plates;
+        this.time = new GregorianCalendar();
+    }
+
+    public int getNumberPeople() {
+        return this.numberPeople;
+    }
+
+    public void setNumberPeople(int numberPeople) {
+        this.numberPeople = numberPeople;
+    }
+
+    public String getPlates() {
+        return this.plates;
+    }
+
+    public void setPlates(String plates) {
+        this.plates = plates;
+    }
+
+    public String getTime() {
+        return this.time.toString();
+    }
+
+    public void setTime(GregorianCalendar gregorianCalendar) {
+        this.time = gregorianCalendar;
+    }
+
+    public JSONObject save() {
+        JSONObject jsonObject = new JSONObject();
+        try {
+            jsonObject.put(NUMBER_PEOPLE, this.numberPeople);
+            jsonObject.putOpt(PLATES, this.plates);
+            jsonObject.putOpt(TIME, this.time);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return jsonObject;
+    }
+
+    @Override
+    public String toString() {
+        return this.save().toString();
+    }
+
+    public static Reservation create(JSONObject jsonObject) {
+        Reservation reservation = new Reservation();
+        try {
+            reservation.setNumberPeople(jsonObject.getInt(NUMBER_PEOPLE));
+            reservation.setPlates(jsonObject.getString(PLATES));
+            reservation.setTime((GregorianCalendar)jsonObject.get(TIME));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+        return reservation;
+    }
+
+    public static Reservation create(String description) {
+        try {
+            return create(new JSONObject(description));
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+}
