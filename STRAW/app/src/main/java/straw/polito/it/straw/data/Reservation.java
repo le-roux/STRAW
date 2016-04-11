1bd4409e4c843cfbd6f5fefbd1269f7bd5af0bdf
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.fr>
2016-04-11 11:01:23 +0200

    Improvement of the display of a reservation

--- STRAW/app/src/main/java/straw/polito/it/straw/data/Reservation.java
+++ STRAW/app/src/main/java/straw/polito/it/straw/data/Reservation.java
@@ -55,13 +55,25 @@ public class Reservation {
     public String getTimeString() {
         String time = "";
         StringBuilder stringBuilder = new StringBuilder();
-        stringBuilder.append(this.time.get(Calendar.DAY_OF_MONTH))
-                .append("/")
-                .append(this.time.get(Calendar.MONTH))
-                .append("  ")
-                .append(this.time.get(Calendar.HOUR_OF_DAY))
-                .append(":")
-                .append(this.time.get(Calendar.MINUTE));
+        int day = this.time.get(Calendar.DAY_OF_MONTH);
+        if (day < 10)
+            stringBuilder.append('0');
+        stringBuilder.append(day)
+                .append("/");
+        int month = this.time.get(Calendar.MONTH);
+        if (month < 10)
+            stringBuilder.append('0');
+        stringBuilder.append(month)
+                .append("  ");
+        int hour = this.time.get(Calendar.HOUR_OF_DAY);
+        if (hour < 10)
+            stringBuilder.append('0');
+        stringBuilder.append(hour)
+                .append(":");
+        int minute = this.time.get(Calendar.MINUTE);
+        if (minute < 10)
+            stringBuilder.append('0');
+        stringBuilder.append(minute);
         return stringBuilder.toString();
     }
 
