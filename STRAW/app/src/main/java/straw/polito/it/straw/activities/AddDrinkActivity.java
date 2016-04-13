aab27d708fb8c4de34bd93603d6a11dfedbe6742
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.fr>
2016-04-13 22:37:39 +0200

    Addition of comments in AddDrinkActivity

--- STRAW/app/src/main/java/straw/polito/it/straw/activities/AddDrinkActivity.java
+++ STRAW/app/src/main/java/straw/polito/it/straw/activities/AddDrinkActivity.java
@@ -31,53 +31,69 @@ import straw.polito.it.straw.data.Food;
 import straw.polito.it.straw.straw.polito.it.straw.utils.ImageManager;
 import straw.polito.it.straw.straw.polito.it.straw.utils.Logger;
 
+/**
+ * This activity allows the manager to create a new drink and to add it to the menu of his/her
+ * restaurant and also to modify an existing drink.
+ */
 public class AddDrinkActivity extends AppCompatActivity {
 
+    //The views
     private TextView title;
     private EditText name_field;
     private EditText price_field;
     private EditText volume_field;
     private Button add_button;
     private ImageView image;
-    private Uri fileUri = null;
-    private Context context;
-    private Drink drink;
-    private SharedPreferences sharedPreferences;
-    private PopupWindow popupWindow;
     private ListView listView;
+    private PopupWindow popupWindow;
+
+    //Data from the model
+    private Uri fileUri = null;
+    private Drink drink;
+
+    //
+    private Context context;
+    private SharedPreferences sharedPreferences;
     private Intent intent;
     private String action;
 
+    //Request codes for the onResult method
     private static final int TAKE_PICTURE_REQUEST_CODE = 1;
     private static final int CHOOSE_PICTURE_REQUEST_CODE = 2;
 
-    private static final String TAG = "FoodApp";
+    //Key for storing/retrieving the data to/from dictionaries
     private static final String DRINK = "Drink";
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_drink);
         this.intent = getIntent();
+
         setPopupWindow();
+
+        //Get the views from the display
         this.title = (TextView)findViewById(R.id.title);
         this.name_field = (EditText)findViewById(R.id.name_field);
         this.price_field = (EditText)findViewById(R.id.price_field);
         this.volume_field = (EditText)findViewById(R.id.volume_field);
         this.add_button = (Button)findViewById(R.id.confirm_button);
         this.image = (ImageView)findViewById(R.id.photo_imageView);
+
         this.context = getApplicationContext();
         this.sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this.context);
+
         if (savedInstanceState != null)
             restoreValues(savedInstanceState.getString(DRINK));
         else {
+            //No temporarily stored data --> retrieve it from the intent that launched the activity
             action = this.intent.getStringExtra(CreateMenuActivity.ACTION);
             if(action.equals(CreateMenuActivity.ADD_ELEMENT)) {
                 this.drink = new Drink();
             } else if (action.equals(CreateMenuActivity.EDIT_ELEMENT)) {
                 String description = this.intent.getStringExtra(CreateMenuActivity.ELEMENT);
                 this.drink = (Drink) Food.create(description);
+                //Change the title of the page to describe properly the current action
                 this.title.setText(getText(R.string.Edit_drink));
             }
         }
@@ -91,22 +107,24 @@ public class AddDrinkActivity extends AppCompatActivity {
             }
         });
 
+        //Add a listener to the "Add" button
         this.add_button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 update();
+                //Save the new drink in permanent storage
                 sharedPreferences.edit().putString(drink.getName(), drink.toString()).commit();
+
+                //Prepare the return to the calling activity
                 Intent result = new Intent(getApplicationContext(), CreateMenuActivity.class);
                 Bundle data = new Bundle();
                 data.putString(CreateMenuActivity.ACTION, action);
-                if (action == CreateMenuActivity.EDIT_ELEMENT) {
+                if (action.equals(CreateMenuActivity.EDIT_ELEMENT)) {
                     data.putString(CreateMenuActivity.ID, intent.getStringExtra(CreateMenuActivity.ID));
                 }
                 data.putString(CreateMenuActivity.ELEMENT, drink.toString());
-                Logger.d("drink stored");
                 result.putExtras(data);
                 setResult(Activity.RESULT_OK, result);
-                Logger.d("result set");
                 finish();
             }
         });
@@ -121,7 +139,7 @@ public class AddDrinkActivity extends AppCompatActivity {
             //In case of "Select picture", the fileUri is already set
             ImageManager.setImage(context, image, fileUri);
         } else if(resultCode == RESULT_CANCELED) {
-            Log.d(TAG, "result canceled");
+            Logger.d("result canceled");
         }
     }
 
@@ -136,6 +154,13 @@ public class AddDrinkActivity extends AppCompatActivity {
         restoreValues(b.getString(DRINK));
     }
 
+    /**
+     * A simple method that takes care of setting the content of all the fields in the activity
+     * according to the description given
+     * @param drink_descriptor String representation of a JSON object describing a drink
+     *                         See the Drink class for a description of the expected fields on this
+     *                         JSON object.
+     */
     private void restoreValues(String drink_descriptor) {
         this.drink = (Drink) Food.create(drink_descriptor);
         this.name_field.setText(this.drink.getName());
@@ -154,53 +179,76 @@ public class AddDrinkActivity extends AppCompatActivity {
      * Update the values in the Drink object according to the fields of the display
      */
     public void update() {
+        //Name
         String name = this.name_field.getText().toString();
         if (name.equals(""))
             this.drink.setName(getText(R.string.Default).toString());
         else
             this.drink.setName(name);
+
+        //Price
         String price = this.price_field.getText().toString();
         if (price.equals(""))
             this.drink.setPrice(0d);
         else
             this.drink.setPrice(Double.parseDouble(price));
+
+        //Volume
         String volume = this.volume_field.getText().toString();
         if (volume.equals(""))
             this.drink.setVolume(0d);
         else
             this.drink.setVolume(Double.parseDouble(volume));
+
+        //Image
         if(this.fileUri != null)
             this.drink.setImageURI(this.fileUri.toString());
         else
             this.drink.setImageURI(null);
     }
 
+    /**
+     * Initialisation of the popup that appears when clicking on the ImageView
+     */
     private void setPopupWindow() {
         this.popupWindow = new PopupWindow(this.getApplicationContext());
+
+        //Create the content of the popup menu
+        //WARNING : when modifying it, don't forget to change also the constants
+        //definition that follows
         ArrayList<String> content = new ArrayList<String>();
         content.add(getString(R.string.Choose_photo));
         content.add(getString(R.string.Take_photo));
-        ArrayAdapter<String> popupAdapter = new ArrayAdapter<String>(this.getApplicationContext(), android.R.layout.simple_list_item_1, content);
+        final int CHOOSE_PHOTO = 0;
+        final int TAKE_PHOTO = 1;
+
+
+        ArrayAdapter<String> popupAdapter = new ArrayAdapter<String>(this.getApplicationContext(),
+                android.R.layout.simple_list_item_1, content);
         this.listView = new ListView(this.getApplicationContext());
         listView.setAdapter(popupAdapter);
+        //Add a listener to react when one of the element of the popup is clicked
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                if (position == 0) {
-                    //Choose photo
+                if (position == CHOOSE_PHOTO) {
                     Intent choosePictureIntent = new Intent(Intent.ACTION_GET_CONTENT);
                     choosePictureIntent.setType("image/*");
                     startActivityForResult(choosePictureIntent, CHOOSE_PICTURE_REQUEST_CODE);
-                } else {
+                } else if (position == TAKE_PHOTO) {
                     Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                    //Take photo
-                    fileUri = ImageManager.getOutputMediaFileUri(context, name_field.getText().toString()); //Create a file to store the photo
-                    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri); //Set the image file name
-                    startActivityForResult(intent, TAKE_PICTURE_REQUEST_CODE); //Launch the camera app
+                    //Create a file to store the photo
+                    fileUri = ImageManager.getOutputMediaFileUri(context, name_field.getText().toString());
+                    //Set the image file name
+                    intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+                    //Launch the camera app
+                    startActivityForResult(intent, TAKE_PICTURE_REQUEST_CODE);
                 }
                 popupWindow.dismiss();
             }
         });
+
+        //Set the appearance and content of the popup menu
         popupWindow.setFocusable(true);
         popupWindow.setWidth(500);
         popupWindow.setHeight(WindowManager.LayoutParams.WRAP_CONTENT);
