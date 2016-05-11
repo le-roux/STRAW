cf579d3537c6806623dbd4e2464069e02715e069
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.fr>
2016-05-12 00:17:02 +0200

    Correction

--- STRAW/app/src/main/java/straw/polito/it/straw/activities/DisplayMenuActivity.java
+++ STRAW/app/src/main/java/straw/polito/it/straw/activities/DisplayMenuActivity.java
@@ -30,7 +30,7 @@ public class DisplayMenuActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_menu);
+        setContentView(R.layout.activity_display_menu);
 
         this.application = (StrawApplication)getApplication();
 
