6ecd3c8260c7dbf3bfbc77c4ca40bf5e61677bb7
Sylvain LE ROUX <sylvain.leroux@telecom-paristech.fr>
2016-06-10 16:51:25 +0200

    Addition of the toolbar in display invitations

--- STRAW/app/src/main/java/straw/polito/it/straw/activities/DisplayInvitationActivity.java
+++ STRAW/app/src/main/java/straw/polito/it/straw/activities/DisplayInvitationActivity.java
@@ -3,6 +3,7 @@ package straw.polito.it.straw.activities;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -19,6 +20,10 @@ public class DisplayInvitationActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display_invitation);
 
+        Toolbar toolbar = (Toolbar)findViewById(R.id.tool_bar); // Attaching the layout to the toolbar object
+        toolbar.setTitle(getString(R.string.Display_Invitations));
+        setSupportActionBar(toolbar);
+
         String message = getIntent().getStringExtra(InviteFriendActivity.INVITATION);
         this.restaurantName = getIntent().getStringExtra(InviteFriendActivity.RESTAURANT);
         if (message == null)
@@ -29,7 +34,7 @@ public class DisplayInvitationActivity extends AppCompatActivity {
 
         if (text != null)
             text.setText(message);
-       if (logIn != null) {
+        if (logIn != null) {
             logIn.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
